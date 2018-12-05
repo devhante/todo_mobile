41f6d01cac4aaa4e551215d85c71270d6a85558a
Kyungwook Cho <amisay5256@naver.com>
2018-12-05 15:44:53 +0900

    [Add] 로그인 화면 디자인

--- App.tsx
+++ App.tsx
@@ -1,25 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    username: '',
+    password: '',
+  }
+
   render() {
+    const handleChangeUsername = (username: string) => {
+      this.setState({username: username});
+    }
+    const handleChangePassword = (password: string) => {
+      this.setState({password: password});
+    }
+    const handlePressLogin = () => {
+
+    }
+    
     return (
       <View style={styles.container}>
         <View style={styles.top}>
-          <Icon style={styles.icon} name="list-ul" size={60} color="#6200EE" />
-          <Text>To-do List App</Text>
+          <Icon style={styles.icon} name="list-ul" size={60} color='#8BE9FD' />
+          <Text style={styles.title}>To-do List App</Text>
         </View>
-        <View style={styles.bottom}></View>
+        <View style={styles.bottom}>
+          <TextInput style={styles.username} placeholder='Username'
+          placeholderTextColor='#F8F8F2' underlineColorAndroid='#F8F8F2'
+          textContentType='username' secureTextEntry={false}
+          value={this.state.username} onChangeText={handleChangeUsername} />
+          <TextInput style={styles.password} placeholder='Password'
+          placeholderTextColor='#F8F8F2' underlineColorAndroid='#F8F8F2'
+          textContentType='password' secureTextEntry={true}
+          value={this.state.password} onChangeText={handleChangePassword} />
+          <Button color="#8BE9FD"  title="Login" onPress={handlePressLogin} />
+        </View>
       </View>
     );
   }
@@ -30,20 +47,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#282E36',
   },
   top: {
     flex: 1,
-    flexGrow: 2,
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   bottom: {
     flex: 1,
-    flexGrow: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexGrow: 1,
+    width: 300,
   },
   icon: {
     margin: 16,
-  }
+  },
+  title: {
+    color: '#F8F8F2',
+  },
+  username: {
+    color: '#F8F8F2',
+    width: 300,
+    marginBottom: 24,
+  },
+  password: {
+    color: '#F8F8F2',
+    width: 300,
+    marginBottom: 48,
+  },
 });
