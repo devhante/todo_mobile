c3126932a7f109a3b9c7bd9eb4ff6a2e0742d6ef
Kyungwook Cho <amisay5256@naver.com>
2018-12-12 15:01:43 +0900

    [Change] serializer를 interface로 변경

--- src/serializer.ts
+++ src/serializer.ts
@@ -1,16 +1,30 @@
-export class UserSerializer {
-    public id = 0;
-    public username = '';
-    public authToken = '';
-    public name = '';
+export interface UserSerializer {
+    id: number;
+    username: string
+    authToken: string;
+    name: string;
 }
 
-export class TodoSerializer {
-    public id = 0;
-    public content = '';
-    public user = new UserSerializer();
-    public like = 0;
-    public createdAt = '';
-    public completedAt = '';
-    public isCompleted = false;
+export class User {
+    id: number;
+    username: string
+    authToken: string;
+    name: string;
+    
+    constructor(value: UserSerializer) {
+        this.id = value.id;
+        this.username = value.username;
+        this.authToken = value.authToken;
+        this.name = value.name;
+    }
+}
+
+export interface TodoSerializer {
+    id: number;
+    content: string;
+    user: UserSerializer;
+    like: number;
+    createdAt: string;
+    completedAt: string;
+    isCompleted: boolean;
 }
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,15 +1,15 @@
 import { action, observable } from "mobx"
-import { UserSerializer } from '../serializer';
+import { UserSerializer, User } from '../serializer';
 import RootStore from "./rootStore";
 
 export default class UserStore {
-    @observable public user = new UserSerializer();
+    @observable public user: User;
     private rootStore: RootStore;
 
     constructor(rootStore: RootStore) {
+        this.user = new User({id: 0, username: '', authToken: '', name: ''});
         this.rootStore = rootStore;
     }
-
 
     @action
     public setUser = (user: UserSerializer) => {
